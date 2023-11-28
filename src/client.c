814279a991e04a822b4d1c9e4e50155513260421
Stephen <ma000094@umn.edu>
2023-11-28 15:28:30 -0600

    added some stuff

--- src/client.c
+++ src/client.c
@@ -5,33 +5,65 @@
 
 int send_file(int socket, const char *filename) {
     // Open the file
+    FILE *fd = fopen(filename, "r");
+    if (fd == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
 
     // Set up the request packet for the server and send it
+    packet_t request_packet;
+    // request_packet.operation = 
+    // request_packet.flags =
+    // request_packet.size = 
+    send(socket, &request_packet, sizeof(packet_t), 0);
 
     // Send the file data
+    
+    fclose(fd);
     return 0;
 }
 
 int receive_file(int socket, const char *filename) {
     // Open the file
+    FILE *fd = fopen(filename, "w");
+    if (fd == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
 
     // Receive response packet
+    packet_t response_packet;
+    recv(socket, &response_packet, sizeof(packet_t), 0);
 
     // Receive the file data
 
     // Write the data to the file
+
+    fclose(fd);
     return 0;
 }
 
 int main(int argc, char* argv[]) {
-    if(argc != 3){
+    if(argc != 4){
         fprintf(stderr, "Usage: ./client File_Path_to_images File_Path_to_output_dir Rotation_angle. \n");
         return 1;
     }
     
     // Set up socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0); // create socket to establish connection
+    if(sockfd == -1)
+        perror("socket error");
+
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // server IP, since the server is on same machine, use localhost IP
+    servaddr.sin_port = htons(PORT); // Port the server is listening on
 
     // Connect the socket
+    int ret = connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)); // establish connection to server
+    if(ret == -1)
+        perror("connect error");
 
     // Read the directory for all the images to rotate
 
@@ -42,7 +74,9 @@ int main(int argc, char* argv[]) {
     // Receive the processed image and save it in the output dir
 
     // Terminate the connection once all images have been processed
+    close(sockfd); // close socket
 
     // Release any resources
+    
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -4,30 +4,69 @@
 #define MAX_CLIENTS 5
 #define BUFFER_SIZE 1024 
 
-
+//Stephen: only have copied stuff from lab 12 onto here, so struct stuff might not match
 void *clientHandler(void *socket) {
 
     // Receive packets from the client
+    char recvdata[PACKETSZ];
+    memset(recvdata, 0, PACKETSZ);
+    ret = recv(conn_fd, recvdata, PACKETSZ, 0);
+    if(ret == -1)
+        perror("recv error");    
 
     // Determine the packet operatation and flags
+    Packet *recvpacket = deserializeData(recvdata);
 
     // Receive the image data using the size
 
     // Process the image data based on the set of flags
 
     // Acknowledge the request and return the processed image data
+    Packet packet;
+    packet.operation = htons(PROTO_ACK);
+    strcpy(packet.data, recvpacket->data);
+    char *serializedData = serializePacket(&packet);
+    ret = send(conn_fd, serializedData, PACKETSZ, 0); 
+    if(ret == -1)
+        perror("send error");
 }
 
 int main(int argc, char* argv[]) {
 
     // Creating socket file descriptor
-
+    int listen_fd, conn_fd;
     // Bind the socket to the port
+
+    /* added by Stephen, still need to work out order of stuff
+    struct sockaddr_in servaddr;
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen to any of the network interface (INADDR_ANY)
+    servaddr.sin_port = htons(PORT); // Port number
+    */
+    int ret = bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)); // bind address, port to socket
+    if(ret == -1)
+        perror("bind error");
 
     // Listen on the socket
 
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0); // create listening socket
+    if(listen_fd == -1)
+        perror("socket error");
+
     // Accept connections and create the client handling threads
 
+    struct sockaddr_in clientaddr;
+    socklen_t clientaddr_len = sizeof(clientaddr);
+    conn_fd = accept(listen_fd, (struct sockaddr *) &clientaddr, &clientaddr_len); // accept a request from a client
+    if(conn_fd == -1)
+        perror("accept error");
+
     // Release any resources
+
+    // close sockets
+    close(conn_fd); 
+    close(listen_fd);
+
     return 0;
 }
