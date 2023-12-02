92ba2e7da3dff69369f14357f88e4debcddb9305
Stephen <ma000094@umn.edu>
2023-12-01 22:23:24 -0600

    changed Packet to packet_t in header files

--- include/client.h
+++ include/client.h
@@ -61,7 +61,7 @@ typedef struct packet {
 } packet_t; 
 
 // serialize packet
-char *serializePacket(Packet *packet){
+char *serializePacket(packet_t *packet){
     char *serializedData = (char *)malloc(sizeof(char) * PACKETSZ);
     memset(serializedData, 0, PACKETSZ);
     memcpy(serializedData, packet, PACKETSZ);
@@ -69,8 +69,8 @@ char *serializePacket(Packet *packet){
 }
 
 // deserialize data
-Packet *deserializeData(char *serializedData){
-    Packet *packet = (Packet *)malloc(sizeof(Packet));
+packet_t *deserializeData(char *serializedData){
+    packet_t *packet = (packet_t *)malloc(sizeof(packet_t));
     memset(packet, 0, PACKETSZ);
     memcpy(packet, serializedData, PACKETSZ);
     return packet;
